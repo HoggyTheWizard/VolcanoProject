b9e1840ee27f6f10bce9f76d6590239982a2e7e6
HoggyTheWizard <hogwartswizrd@icloud.com>
2022-02-17 08:13:15 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public float speed; 
+    private void Update()
+    {
+        if (Input.GetKey("a") && transform.position.x > -8)
+        {
+            transform.position += speed * Time.deltaTime * new Vector3(-1f, 0f, 0f); 
+        }
+
+        else if (Input.GetKey("d") && transform.position.x < 8)
+        {
+            transform.position += speed * Time.deltaTime * new Vector3(1f, 0f, 0f);
+        }
+        
+    }
+}
